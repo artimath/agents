af432a1b4fe05c71603186c92fbd42affed1be45
Ryan Hunter <your-email@example.com>
2025-03-01 18:21:17 -0800

    update deps

--- guides/human-in-the-loop/src/utils.ts
+++ guides/human-in-the-loop/src/utils.ts
@@ -1,9 +1,11 @@
-import { formatDataStreamPart, type Message } from "@ai-sdk/ui-utils";
+
 import {
   convertToCoreMessages,
   type DataStreamWriter,
   type ToolExecutionOptions,
   type ToolSet,
+  type Message,
+  formatDataStreamPart,
 } from "ai";
 import { z } from "zod";
 
